4e0f4a6415e8df5e87ba928d35b7065c261745fd
kewenkang <1528428728@qq.com>
2017-07-03 18:34:54 +0800

    config-2

---
+++ src/main/java/com/im/actor/model/Actor.java
@@ -0,0 +1,7 @@
+package com.im.actor.model;
+
+/**
+ * Created by lenovo on 2017/7/3.
+ */
+public class Actor {
+}
---
+++ src/main/java/com/im/filter/AuthorityFilter.java
@@ -0,0 +1,24 @@
+package com.im.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by lenovo on 2017/7/3.
+ */
+public class AuthorityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
