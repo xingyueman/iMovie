09da480d6aa5fc2dff9a6211f0d9b4ab43d3f573
kewenkang <1528428728@qq.com>
2017-07-04 08:58:14 +0800

    model-1

---
+++ src/main/java/com/im/category/model/Category.java
@@ -0,0 +1,7 @@
+package com.im.category.model;
+
+/**
+ * Created by lenovo on 2017/7/4.
+ */
+public class Category {
+}
---
+++ src/main/java/com/im/user/model/User.java
@@ -0,0 +1,7 @@
+package com.im.user.model;
+
+/**
+ * Created by lenovo on 2017/7/4.
+ */
+public class User {
+}
